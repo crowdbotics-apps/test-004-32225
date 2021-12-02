961b0ad63248b965085a16d3dd515b84af7083ae
Crowdbotics <team@crowdbotics.com>
2021-12-02 22:31:27 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,13 @@
+from django.conf import settings
 from django.db import models
+
+
+class Auto(models.Model):
+    "Generated Model"
+    brand = models.CharField(
+        max_length=256,
+    )
+    fourwheeldrive = models.BooleanField(
+        null=True,
+        blank=True,
+    )
